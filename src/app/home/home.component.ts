0340b69b8b8d604f8f79f3d1e14d46e9893bb707
Bazabas Barbara <barbara-bazabas@live.fr>
2023-06-25 21:10:34 +0100

    lesson 13 done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,15 +14,12 @@ import { HousingService } from '../housing.service';
     template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city, state or name" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
-<app-housing-location
-  *ngFor="let housingLocation of housingLocationList"
-  [housingLocation]="housingLocation">
-</app-housing-location>
+<app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
 </section>
   `,
     styleUrls: ['./home.component.css']
@@ -30,9 +27,36 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
     housingLocationList: HousingLocation[] = [];
     housingService: HousingService = inject(HousingService);
+    filteredLocationList: HousingLocation[] = [];
 
+    filterResults(text: string) {
+        if (!text) {
+            this.filteredLocationList = this.housingLocationList;
+        }
+        this.filteredLocationList = this.housingLocationList.filter(
+            housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()) ||
+                housingLocation?.name.toLowerCase().includes(text.toLowerCase()) ||
+                housingLocation?.state.toLowerCase().includes(text.toLowerCase())
+        );
+    }
+    // filterStateResults(text: string) {
+    //     if (!text) {
+    //         this.filteredLocationListState = this.housingLocationList;
+    //     }
+    //     this.filteredLocationListState = this.housingLocationList.filter(
+    //         housingLocation => housingLocation?.name.toLowerCase().includes(text.toLowerCase())
+    //     );
+    // }
+    // filterNameResults(text: string) {
+    //     if (!text) {
+    //         this.filteredLocationListName = this.housingLocationList;
+    //     }
+    //     this.filteredLocationListName = this.housingLocationList.filter(
+    //         housingLocation => housingLocation?.name.toLowerCase().includes(text.toLowerCase())
+    //     );
+    // }
     constructor() {
         this.housingLocationList = this.housingService.getAllHousingLocations();
+        this.filteredLocationList = this.housingLocationList;
     }
-
 }
